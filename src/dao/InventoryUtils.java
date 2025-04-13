577e2400232823e5c595f24358013bf2991c83ac
RamyaJ09 <joshiramya7@gmail.com>
2025-04-13 11:39:43 +0530

    Initial commit

---
+++ src/dao/InventoryUtils.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author Gurur
+ */
+public class InventoryUtils {
+    
+    public static String billPath = "D:\\";
+}
